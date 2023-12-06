76ae6236ed2e37e2639afbb78b4e4ee13b2d3994
pashute <pashute@gmail.com>
2023-12-06 22:45:48 +0200

    electron ready

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('Hello from Electron 👋')
+// console.log('Hello from Electron')
+
+// CommonJS module syntax:Import app and BrowserWindow 
+// app - app's event lifecycle. Non instatiable
+// BrowserWindow - creates and manages app windows
+const { app, BrowserWindow } = require('electron/main')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+  })
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+  })
+
